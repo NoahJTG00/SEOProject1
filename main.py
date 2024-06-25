0066203e2b1080d87372df40d7c25af985c56d9d
NoahJTG00 <njt2129@columbia.edu>
2024-06-25 15:45:27 -0400

    Co-authored-by: immanuella1 <129225655+immanuella1@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,38 @@
+# Main file where everything is ran from
+import sqlite3
+from user import *
+from insertData import *
+from database import *
+
+conn = sqlite3.connect('langhelp.db')
+
+# Create a cursor object to interact with the database
+cur = conn.cursor()
+
+# Create table for users and languages
+# createTables()
+
+# # Insert language data
+# insertData()
+
+# Query to get all unique languages
+cur.execute('SELECT DISTINCT language FROM language')
+
+# Fetch all unique languages
+languages = cur.fetchall()
+
+# Print the list of languages
+for language in languages:
+    print(language[0])
+
+
+# Get information from user and insert into table
+insertUser()
+
+
+
+# Commit the changes and close the connection
+conn.close()
+
+
+
